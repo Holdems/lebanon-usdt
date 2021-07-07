137b6e9fd89ddf9998c4b28a7c509c5b30f8eef3
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-07-07 14:16:40 +0300

    created UI to view sell and buy requests

--- lib/buy_requests_page.dart
+++ lib/buy_requests_page.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:lebanon_usdt/models/request/buy_request.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import  'package:http/http.dart' as http;
 
+int b = 0;
 class BuyRequestsPage extends StatefulWidget {
   const BuyRequestsPage({Key? key}) : super(key: key);
 
@@ -14,30 +16,35 @@ class BuyRequestsPage extends StatefulWidget {
 class _BuyRequestsPageState extends State<BuyRequestsPage> {
 
   // https://lebanon-usdt.azurewebsites.net/br/
-
   Future<List<BuyRequest>> getBuyRequests() async {
-    var uri =
-    Uri(scheme: 'https', host: '//lebanon-usdt.azurewebsites.net', path: '/br/');
-    var data = await http.get(uri, headers: {
+    var url = Uri.parse('https://lebanon-usdt.azurewebsites.net/br/');
+    http.Response response = await http.get(url, headers: {
       "content-type": "application/json",
     });
-    var jsonData = json.decode(data.body);
+    print(1);
+    print(response.statusCode);
+    var jsonData = json.decode(response.body);
+    print(jsonData);
     List<BuyRequest> requests = [];
     for (var request in jsonData) {
       BuyRequest _request = BuyRequest(
-        request['rate'],
+        request['id'],
+        request['user'],
         request['amount'],
+        request['rate'],
         request['total_price'],
-        request['user']
       );
       requests.add(_request);
     }
+    print(requests);
     return requests;
   }
 
 
   @override
   Widget build(BuildContext context) {
+    b +=1;
+    print("t is now $b");
     return Container(
       child: FutureBuilder(
         future: getBuyRequests(),
@@ -46,15 +53,40 @@ class _BuyRequestsPageState extends State<BuyRequestsPage> {
             return Container(
               child: Center(
                   child: SpinKitCircle(
-                    color: Colors.greenAccent,
+                    color: Colors.lightBlueAccent,
                   )),
             );
           }
           else {
-            return Card(
-              child: ListTile(
-                leading: snapshot.data['rate'],
-              ),
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index){
+                return Column(
+                  children: [
+                    SizedBox(height: 10,),
+                    Card(
+                      color: Colors.teal,
+                      shadowColor: Colors.blueAccent,
+                      child: InkWell(
+                        child: SizedBox(
+                          height: 100,
+                          width: 300,
+                          child: Column(
+                            children: [
+                              Text(snapshot.data[index].id.toString()),
+                              Text(snapshot.data[index].username),
+                              Text(snapshot.data[index].amount.toString()),
+                              Text(snapshot.data[index].rate.toString()),
+                              Text(snapshot.data[index].totalPrice.toString()),
+                              Text(snapshot.data[index].toStringType()),
+                            ],
+                          ),
+                        ),
+                      )
+                    )
+                  ],
+                );
+              }
             );
           }
         }
@@ -62,5 +94,3 @@ class _BuyRequestsPageState extends State<BuyRequestsPage> {
     );
   }
 }
-
-
--- lib/sell_requests_page.dart
+++ lib/sell_requests_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:lebanon_usdt/models/request/sell_request.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+int s = 0;
 
 class SellRequestsPage extends StatefulWidget {
   const SellRequestsPage({Key? key}) : super(key: key);
@@ -8,8 +15,88 @@ class SellRequestsPage extends StatefulWidget {
 }
 
 class _SellRequestsPageState extends State<SellRequestsPage> {
+  // https://lebanon-usdt.azurewebsites.net/sr/
+
+  Future<List<SellRequest>> getSellRequests() async {
+    var url = Uri.parse('https://lebanon-usdt.azurewebsites.net/sr/');
+    http.Response response = await http.get(url, headers: {
+      "content-type": "application/json",
+    });
+    print(1);
+    print(response.statusCode);
+    var jsonData = json.decode(response.body);
+    print(jsonData);
+    List<SellRequest> requests = [];
+    for (var request in jsonData) {
+      SellRequest _request = SellRequest(
+        request['id'],
+        request['user'],
+        request['amount'],
+        request['rate'],
+        request['total_price'],
+      );
+      requests.add(_request);
+    }
+    print(requests);
+    return requests;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    s+=1;
+    print("s is now $s");
+    return Container(
+      child: FutureBuilder(
+          future: getSellRequests(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                    child: SpinKitCircle(
+                      color: Colors.lightBlueAccent,
+                    )),
+              );
+            }
+            else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Card(
+                              color: Colors.lightBlueAccent,
+                              shadowColor: Colors.blueAccent,
+                              child: ExpansionTile(
+                                leading: Text("Selling"),
+                                title: Text("100 USDT\nBeirut"),
+                                subtitle: Text("+ 1.6%"),
+                                // trailing: Text("s"),
+                                children: [
+                                  Text("Hello"),
+                                  Text("ss"),
+                                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                    ElevatedButton(onPressed: (){}, child: Row(
+                                      children: [
+                                        Icon(Icons.chat),
+                                        Text(" Message on WhatsApp"),
+                                      ],
+                                    ))
+                                  ],)
+                                ],
+                              )
+                          ),
+                        )
+                      ],
+                    );
+                  }
+              );
+            }
+          }
+      ),
+    );
   }
 }
