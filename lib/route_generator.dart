ad8591f6d5e4ec3929a9223813b77f789c0d3e49
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-08-06 03:28:01 +0300

    updated route_generator.dart with new routes

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -4,6 +4,10 @@ import 'package:lebanon_usdt/home.dart';
 import 'package:lebanon_usdt/login.dart';
 import 'package:lebanon_usdt/make_request_screen.dart';
 import 'package:lebanon_usdt/requests_screen.dart';
+import 'package:lebanon_usdt/screens/login/login_screen.dart';
+import 'package:lebanon_usdt/screens/signup/signup_screen.dart';
+import 'package:lebanon_usdt/screens/welcome/welcome_screen.dart';
+import 'package:lebanon_usdt/test.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -12,15 +16,26 @@ class RouteGenerator {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => HomeScreen());
+      case '/welcomeScreen':
+        return MaterialPageRoute(builder: (_) => WelcomeScreen());
+      case '/homeScreen':
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+      case '/signUpScreen':
+        return MaterialPageRoute(builder: (_) => SignUpScreen());
+      case '/loginScreen':
+        return MaterialPageRoute(builder: (_) => LoginScreen());
       case '/makeRequest':
         var data = settings.arguments as String;
-        return MaterialPageRoute(builder: (_) => MakeRequestScreen(title: data));
+        return MaterialPageRoute(
+            builder: (_) => MakeRequestScreen(title: data));
       case '/home':
         return MaterialPageRoute(builder: (_) => Home());
       case '/login':
-        return MaterialPageRoute(builder: (_)=>Login());
+        return MaterialPageRoute(builder: (_) => Login());
       case '/filter':
-        return MaterialPageRoute(builder: (_)=>Filter());
+        return MaterialPageRoute(builder: (_) => Filter());
+      case '/test':
+        return MaterialPageRoute(builder: (_) => Test());
       //Passing arguments from screen to another
       // case '/merchantHomeScreen':
       //   return MaterialPageRoute(
