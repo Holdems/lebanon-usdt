6bea40b58a37a461c9b7dede6dfc3ec4c1302f09
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-08-06 03:25:15 +0300

    added colors.dart for app colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 //app colors
-Color primaryColor = Colors.white;
-Color secondaryColor = Colors.grey;
+const Color s = Color(0xd91cd0a3);
+const Color primaryColor = Color(0xff1cd0a3);
+const Color primaryLightColor = Color(0xff36e786);
+const Color secondaryColor = Color(0xfff3f3f3);
