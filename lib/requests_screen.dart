92f9b78487388609f20a59c44211f0b99adb7503
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-08-06 03:30:13 +0300

    added animation to sign out button

--- lib/requests_screen.dart
+++ lib/requests_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lebanon_usdt/colors.dart';
 import 'package:lebanon_usdt/components/expandable_fab.dart';
+import 'package:lebanon_usdt/constants.dart';
+import 'package:lebanon_usdt/screens/welcome/welcome_screen.dart';
 import 'package:lebanon_usdt/sell_requests_page.dart';
 
 import 'buy_requests_page.dart';
@@ -43,13 +46,14 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           actions: [
             IconButton(
+              tooltip: "Filter",
               onPressed: () {
                 Navigator.pushNamed(context, '/filter');
               },
               icon: Icon(Icons.filter_alt),
             )
           ],
-          backgroundColor: Color(0xfff3f3f3),
+          backgroundColor: secondaryColor,
           // title: Text("Buy here",style: TextStyle(color: Colors.grey),),
           // shape: Border(bottom: BorderSide(color: Colors.orange, width: 20)),
           //     bottom: PreferredSize(
@@ -61,11 +65,17 @@ class _HomeScreenState extends State<HomeScreen> {
           distance: 112,
           children: [
             ActionButton(
-                icon: const Icon(Icons.sell),
+                icon: const Icon(
+                  Icons.sell,
+                  color: secondaryColor,
+                ),
                 onPressed: () => Navigator.pushNamed(context, '/makeRequest',
                     arguments: 'sell')),
             ActionButton(
-                icon: const Icon(Icons.shopping_bag),
+                icon: const Icon(
+                  Icons.shopping_bag,
+                  color: secondaryColor,
+                ),
                 onPressed: () => Navigator.pushNamed(context, '/makeRequest',
                     arguments: 'buy')),
             // ActionButton(
@@ -85,8 +95,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     end: Alignment(
                         0.8, 0.0), // 10% of the width, so there are ten blinds.
                     colors: <Color>[
-                      Color(0xff1cd0a3),
-                      Color(0xff36e786)
+                      primaryColor,
+                      primaryLightColor
                     ], // red to yellow
                     tileMode: TileMode
                         .repeated, // repeats the gradient over the canvas
@@ -97,7 +107,26 @@ class _HomeScreenState extends State<HomeScreen> {
               // ListTile(title: Text('B')),
               SizedBox(height: 300),
               TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        PageRouteBuilder(pageBuilder: (BuildContext context,
+                            Animation animation, Animation secondaryAnimation) {
+                          return WelcomeScreen();
+                        }, transitionsBuilder: (BuildContext context,
+                            Animation<double> animation,
+                            Animation<double> secondaryAnimation,
+                            Widget child) {
+                          return new SlideTransition(
+                            position: new Tween<Offset>(
+                              begin: const Offset(1.0, 0.0),
+                              end: Offset.zero,
+                            ).animate(animation),
+                            child: child,
+                          );
+                        }),
+                        (Route route) => false);
+                  },
                   child: Text('Sign out', style: TextStyle(fontSize: 20))),
             ],
           ),
@@ -109,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen> {
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _selectedIndex,
           //0xfff3f3f3
-          backgroundColor: Color(0xfff3f3f3),
+          backgroundColor: secondaryColor,
           items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.shopping_bag),
@@ -125,7 +154,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 icon: Icon(Icons.task), label: "My Requests")
           ],
           onTap: _onItemTapped,
-          selectedItemColor: Color(0xff1cd0a3),
+          selectedItemColor: primaryColor,
         ),
       ),
     );
